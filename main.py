72fd0f76e05dd9d8073536f02a1eda2298df47f1
Mixcro <mixcro.chn@gmail.com>
2019-02-25 23:42:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # -*- coding: UTF-8 -*-
-#!/usr/bin/python3
+#!/usr/bin/env python3
 from spider import *
 from file import *
 import threading
 
 
-def updateDatabasePool(timeDelta):
-    '''生成timedelta(日)前的小说idPool'''
+def updateNovel(timeDelta=7, threadNum=128):
+    '''抓取timeDelta日前的小说Index'''
     print('init book list.')
     pool = []
     today = datetime.date.today()
@@ -15,14 +15,11 @@ def updateDatabasePool(timeDelta):
     for book in Book.select().where(Book.lastChapterTime > deadline):
         pool.append(book.id)
     db.close()
-    return(pool)
-
-
-def getIndex(pool, threadNum=16):
-    '''多线程抓取pool中ID的Index并更新database'''
+    print('待抓取Index数: %s'%len(pool))
     poolLock = threading.Lock()
     dbLock = threading.Lock()
     print('init threads.')
+    
     class SpiderThread(threading.Thread):
         def __init__(self, tid):
             threading.Thread.__init__(self)
@@ -36,6 +33,7 @@ def getIndex(pool, threadNum=16):
                 poolLock.release()
                 spider = Spider(bookId)
                 try:
+                    lastDate = idSearch(bookId)[10]
                     spider.get_index()
                 except AttributeError:
                     print('INVALID ID : %s'%bookId)
@@ -45,7 +43,7 @@ def getIndex(pool, threadNum=16):
                     pool.append(bookId)
                     continue
                 else:
-                    if spider.index:
+                    if spider.lastChapterTime > lastDate:
                         item = [spider.id, spider.category, spider.name, spider.author, spider.status, spider.description, 
                                 spider.lastChapterId, spider.lastChapterName, spider.lastChapterTime, spider.index_count]
                         dbLock.acquire()
@@ -54,9 +52,9 @@ def getIndex(pool, threadNum=16):
                         except:
                             addBookList(item)
                         dbLock.release()
-                        print('%s Thread%s done %s_%s'%(time.ctime(time.time()), self.id, bookId, len(pool)))
+                        print('Index已更新: %s'%bookId)
                     else:
-                        addLog(Bpoll, bookId)
+                        print('Index无更新: %s'%bookId)
     
     for i in range(0,threadNum):
         locals()['Thread_%s'%i] = SpiderThread(i)
@@ -66,55 +64,57 @@ def getIndex(pool, threadNum=16):
     for i in range(0,threadNum):
         locals()['Thread_%s'%i].join()
 
-
-class Download_book(threading.Thread):
-    '''抓取idPool中的小说内容至本地'''
-    def __init__(self, tid, pageThreadNumber):
-        threading.Thread.__init__(self)
-        self.id = tid
-        self.pageThreadNumber = pageThreadNumber
-        
-    def run(self):
-        global doList, pool_lock
-        while doList != 0 :
-            pool_lock.acquire()
-            item = doList.pop(0)
-            pool_lock.release()
-            try:
-                spider = Spider(item)
-                try:
-                    spider.get_index()
-                except:
-                    spider.get_index_local()
-                spider.get_cont(self.pageThreadNumber)
-                if spider.index == True:
-                    completeLocal(item)
-                    print('Done %s_%s'%(len(doList), item))
-                    #time.sleep(2)
-            except Execption as e:
-                pool_lock.acquire()
-                doList.append(item)
-                pool_lock.release()
-                print('Get Index Err %s, %s'%(item, e))
-                time.sleep(10)
-
-if __name__ == '__main__':
-    print('Hello world!')
+def downloadAll(thread_number=8, pageThreadNumber=16):
+    '''下载所有local标记为false的小说'''
     print('init db list')
-    thread_number = 32
-    pageThreadNumber = 8
     doList = []
     db.connect()
-    for book in Book.select().where(Book.chapterNumber>0).where(Book.local==False).order_by(Book.chapterNumber):
+    for book in Book.select().where(Book.local==False).order_by(Book.chapterNumber):
        doList.append(book.id)
     db.close()
-    print(len(doList))
+    print('待抓取小说数: %s'%len(doList))
     pool_lock = threading.Lock()
+    
+    class Download_book(threading.Thread):
+        '''抓取idPool中的小说内容至本地'''
+        def __init__(self, tid, pageThreadNumber):
+            threading.Thread.__init__(self)
+            self.id = tid
+            self.pageThreadNumber = pageThreadNumber
+            
+        def run(self):
+            nonlocal doList, pool_lock
+            while doList != 0 :
+                pool_lock.acquire()
+                item = doList.pop(0)
+                pool_lock.release()
+                try:
+                    spider = Spider(item)
+                    try:
+                        spider.get_index()
+                    except:
+                        spider.get_index_local()
+                    spider.get_cont(self.pageThreadNumber)
+                    if spider.index == True:
+                        completeLocal(item)
+                        print('Done %s_%s'%(len(doList), item))
+                        #time.sleep(2)
+                except Execption as e:
+                    pool_lock.acquire()
+                    doList.append(item)
+                    pool_lock.release()
+                    print('Get Index Err %s, %s'%(item, e))
+                    time.sleep(5)
+                    
     for i in range(0,thread_number):
         locals()['Download_%s'%i] = Download_book(i, pageThreadNumber)
-    print('start threads.')
+    print('start threads...')
     for i in range(0,thread_number):
         locals()['Download_%s'%i].start()
     for i in range(0,thread_number):
             locals()['Download_%s'%i].join()
+                
+if __name__ == '__main__':
+    print('Hello world!')
+    updateNovel(14)
     print('Bye world!')
