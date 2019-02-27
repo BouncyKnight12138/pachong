d8faaf74fd03d6343cc93b33f4e7b0630a19f513
Mixcro <mixcro.chn@gmail.com>
2019-02-27 23:26:03 +0800

    Update main.py
    增加了本地文件校验；

--- main.py
+++ main.py
@@ -19,18 +19,17 @@ def updateNovel(timeDelta=7, threadNum=128):
     poolLock = threading.Lock()
     dbLock = threading.Lock()
     print('init threads.')
-    
+
     class SpiderThread(threading.Thread):
         def __init__(self, tid):
             threading.Thread.__init__(self)
             self.id = tid
-            
+
         def run(self):
             nonlocal pool, poolLock, dbLock
             while pool != []:
-                poolLock.acquire()
-                bookId = pool.pop(0)
-                poolLock.release()
+                with poolLock:
+                    bookId = pool.pop(0)
                 spider = Spider(bookId)
                 try:
                     lastDate = idSearch(bookId)[10]
@@ -44,18 +43,17 @@ def updateNovel(timeDelta=7, threadNum=128):
                     continue
                 else:
                     if spider.lastChapterTime > lastDate:
-                        item = [spider.id, spider.category, spider.name, spider.author, spider.status, spider.description, 
+                        item = [spider.id, spider.category, spider.name, spider.author, spider.status, spider.description,
                                 spider.lastChapterId, spider.lastChapterName, spider.lastChapterTime, spider.index_count]
-                        dbLock.acquire()
-                        try:
-                            addItemDB(item)
-                        except:
-                            addBookList(item)
-                        dbLock.release()
+                        with dbLock:
+                            try:
+                                addItemDB(item)
+                            except:
+                                addLog('bookList', item)
                         print('Index已更新: %s'%bookId)
                     else:
                         print('Index无更新: %s'%bookId)
-    
+
     for i in range(0,threadNum):
         locals()['Thread_%s'%i] = SpiderThread(i)
     print('start threads.')
@@ -79,12 +77,12 @@ def rebuildIndex(end=160000, threadNum=128):
     poolLock = threading.Lock()
     dbLock = threading.Lock()
     print('init threads.')
-    
+
     class SpiderThread(threading.Thread):
         def __init__(self, tid):
             threading.Thread.__init__(self)
             self.id = tid
-            
+
         def run(self):
             nonlocal pool, poolLock, dbLock
             while pool != []:
@@ -99,14 +97,14 @@ def rebuildIndex(end=160000, threadNum=128):
                     print('ERR %s_%s'%(bookId, e))
                     pool.append(bookId)
                 else:
-                    item = [spider.id, spider.category, spider.name, spider.author, spider.status, spider.description, 
+                    item = [spider.id, spider.category, spider.name, spider.author, spider.status, spider.description,
                             spider.lastChapterId, spider.lastChapterName, spider.lastChapterTime, spider.index_count]
                     with dbLock:
                         try:
                             addItemDB(item)
                         except:
-                            addBookList(item)
-    
+                            addLog('BookList', item)
+
     for i in range(0,threadNum):
         locals()['Thread_%s'%i] = SpiderThread(i)
     print('start threads.')
@@ -114,7 +112,7 @@ def rebuildIndex(end=160000, threadNum=128):
         locals()['Thread_%s'%i].start()
     for i in range(0,threadNum):
         locals()['Thread_%s'%i].join()
-        
+
 def downloadAll(thread_number=8, pageThreadNumber=16):
     '''下载所有local标记为false的小说'''
     print('init db list')
@@ -125,20 +123,19 @@ def downloadAll(thread_number=8, pageThreadNumber=16):
     db.close()
     print('待抓取小说数: %s'%len(doList))
     pool_lock = threading.Lock()
-    
+
     class Download_book(threading.Thread):
         '''抓取idPool中的小说内容至本地'''
         def __init__(self, tid, pageThreadNumber):
             threading.Thread.__init__(self)
             self.id = tid
             self.pageThreadNumber = pageThreadNumber
-            
+
         def run(self):
             nonlocal doList, pool_lock
             while doList != 0 :
-                pool_lock.acquire()
-                item = doList.pop(0)
-                pool_lock.release()
+                with pool_lock:
+                    item = doList.pop(0)
                 try:
                     spider = Spider(item)
                     try:
@@ -147,16 +144,15 @@ def downloadAll(thread_number=8, pageThreadNumber=16):
                         spider.get_index_local()
                     spider.get_cont(self.pageThreadNumber)
                     if spider.index == True:
-                        completeLocal(item)
+                        changeLocalStatus(item, True)
                         print('Done %s_%s'%(len(doList), item))
                         #time.sleep(2)
-                except Execption as e:
-                    pool_lock.acquire()
-                    doList.append(item)
-                    pool_lock.release()
+                except Exception as e:
+                    with pool_lock:
+                        doList.append(item)
                     print('Get Index Err %s, %s'%(item, e))
                     time.sleep(5)
-                    
+
     for i in range(0,thread_number):
         locals()['Download_%s'%i] = Download_book(i, pageThreadNumber)
     print('start threads...')
@@ -164,8 +160,26 @@ def downloadAll(thread_number=8, pageThreadNumber=16):
         locals()['Download_%s'%i].start()
     for i in range(0,thread_number):
             locals()['Download_%s'%i].join()
-                
+
+def checkCache():
+    '''校验所有本地缓存文件'''
+    pool = []
+    db.connect()
+    for book in Book.select():
+       pool.append((book.id, book.name))
+    db.close()
+    for item in pool:
+        id, name = item
+        try:
+            index_name, index_cont_name, index_cont_id = getLocalIndex(id, name)
+            check(id, name, index_name, index_cont_name, index_cont_id)
+            print('  DONE %s_%s '%(id,name))
+        except Exception as e:
+            changeLocalStatus(id, False)
+            print('ERR: %s_%s'%(id, e))
+
+
 if __name__ == '__main__':
     print('Hello world!')
-    rebuildIndex()
+    downloadAll(1, 128)
     print('Bye world!')
