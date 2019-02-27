a381d4bca1bb62ba18d8507ce9e8b91ee78e4efd
Mixcro <mixcro.chn@gmail.com>
2019-02-27 22:55:49 +0800

    change name

--- test/json_gz.py
+++ test/json_gz.py
@@ -7,6 +7,7 @@ import os
 import re
 import threading
 import time
+from file import *
 
 
 
@@ -19,10 +20,10 @@ def transfer(thread_number=32):
     print(len(book_pool))
 
     class LThread(threading.Thread):
-    
+
         def __init__(self):
             threading.Thread.__init__(self)
-            
+
         def run(self):
             nonlocal book_pool, book_pool_lock
             while book_pool != []:
@@ -43,7 +44,7 @@ def transfer(thread_number=32):
                     index_path = os.path.join(book_path, item)
                     with open(index_path, "r", encoding="utf-8") as f:
                         index[item[:-4]] = eval(f.read())
-                with gzip.open('%s/index.json.gz'%book_path, 'wt', encoding='utf-8',compresslevel=9) as f:
+                with gzip.open('%s/index.json.gz'%book_path, 'wt', encoding='utf-8', compresslevel=9) as f:
                     json.dump(index, f, ensure_ascii=False)
                 # 处理章节内容
                 for chapter in chapter_pool:
@@ -58,11 +59,11 @@ def transfer(thread_number=32):
                             section_content = f.read()
                         content[section_id] = [section_name, section_content]
                     chapter = re.sub('《[\s\S]*》', '', chapter)
-                    with gzip.open('%s/%s.json.gz'%(book_path, chapter), 'wt', encoding='utf-8',compresslevel=9) as f:
+                    with gzip.open('%s/%s.json.gz'%(book_path, chapter), 'wt', encoding='utf-8', compresslevel=9) as f:
                         json.dump(content, f, ensure_ascii=False)
                 # 删除原内容
                 for item in del_pool:
-                    os.system('rm -rf %s/%s'%(book_path, item))
+                    os.system('rm -rf "%s/%s"'%(book_path, item))
                 print('Book: %s'%book)
 
     for i in range(0,thread_number):
@@ -71,9 +72,65 @@ def transfer(thread_number=32):
     for i in range(0,thread_number):
         locals()['Thread_%s'%i].start()
     for i in range(0,thread_number):
-        locals()['Thread_%s'%i].join()  
+        locals()['Thread_%s'%i].join()
+
+def transfer2():
+    '''transfer功能补充'''
+    # 1.delete '《', '》' in the book name;
+    # 2.delete '《', '》' in the index.json.gz.index_name;
+    # 3.delete nongz file in the sub_path;
+    # 4.update the book name in the database;
+
+    path = './book'
+    book_pool = os.listdir(path)
+    book_pool.sort()
+    for book in book_pool:
+
+
+        # fuc 1
+        if book != re.sub(r'[《》]', '' , book):
+            os.system('mv "%s/%s" "%s/%s"'%(path, book, path, re.sub(r'[《》]', '' , book)))
+            book = re.sub(r'[《》]', '' , book)
+
+            # fuc 4
+            db.connect()
+            item = Book.get(Book.id == int(book[:7]))
+            item.name = book[8:]
+            item.save()
+            db.close()
+            print('name clean, ', end='')
+        else:
+            print('name pass,  ', end='')
+
+        # fuc 3
+        sub_path = os.path.join(path, book)
+        sub_pool = os.listdir(sub_path)
+        for item in sub_pool:
+            if item[-2:] != 'gz':
+                os.system('rm -rf "%s/%s"'%(sub_path, item))
+                print('file clean, ', end='')
+
+        # fuc 2
+        with gzip.open('%s/index.json.gz'%sub_path, 'rt', encoding='utf-8') as f:
+            content = json.load(f)
+        index_name = content['index_name']
+        new_index_name = []
+        for item in index_name:
+            new_index_name.append(re.sub('《[\s\S]*》', '' , item))
+        if new_index_name != index_name:
+            content['index_name'] = new_index_name
+            with gzip.open('%s/index.json.gz'%sub_path, 'wt', encoding='utf-8', compresslevel=9) as f:
+                json.dump(content, f, ensure_ascii=False)
+            print('index clean: ', end='')
+        else:
+            print('index pass:  ', end='')
+
+        print('%s: '%book)
+
+
+
 
 if __name__ == '__main__':
     print('Hello world')
-    transfer()
+    transfer2()
     print('Bye world')
